2c4bc2a2c2be9ffcd4cc9033d284e7b3493deec9
Mukesh Jaiswal <56516302+MukeshJaiswal01@users.noreply.github.com>
2020-10-01 13:42:51 +0530

    Update Union.sol

--- Union.sol
+++ Union.sol
@@ -1,19 +1,30 @@
 pragma solidity 0.6.0;
 
 contract Union{
-    address payable public Leader;
-    address  _contract;
-    address payable token ;
-    address payable [] public participants;
-    uint time;
+    address payable public Leader;  // person who will be leader of the community
+                        
+    address  _contract;            // address of the deployed contract
+    
+    address payable token ;       // address of the token that has to be distributed 
+    
+    address payable [] public participants;  // array of  community members 
+    
+    uint time;                               // Time alloted for the deposit into the pot
     uint length = 0;
-    struct  Details {
+    
+    struct  Details {           
+                                  // it will keep the Details of member whether they have 
+                                  // been benefitted or not 
+                                  // it also record the time of transaction
         
         bool benfitted;
         uint time;
     }
     
     mapping(address => Details) Member_detail;
+                        
+                         // map the addres of community member to the Details so that 
+                         // we can keep up with Details of particular member
     
     
     constructor (uint _time,  address payable _token, address payable [] memory _participants ) public {
@@ -26,10 +37,20 @@ contract Union{
     
     modifier onlyLeader(){
         
+                            // only leader  can call particular function like withdrawal()
+        
         require(msg.sender == Leader, "caller is not a Leader");
         _;
     }
+    
+    
     modifier onlyParticipants(){
+        
+        
+                              // only participants are allowed to do specific task 
+                              // like adding the participants
+                              
+                              
         for(uint i = 0; i < participants.length; i++){
              
               if(msg.sender != participants[i] ){
@@ -42,6 +63,11 @@ contract Union{
     
     fallback () payable external {
         
+        
+               // if sender is not in participants list it will be added in list as a member
+               // of the community
+               
+        
         for(uint i = 0; i < participants.length; i++){
             
             if(msg.sender == participants[i]){
@@ -50,7 +76,7 @@ contract Union{
             
             else{
                 
-               participants[length - 1] = msg.sender;
+                participants.push(msg.sender);
                 
             }
         }
@@ -60,12 +86,26 @@ contract Union{
     }
     
     function addParticipants(address payable _member)public onlyParticipants{
-        participants[participants.length - 1] = _member;
+        
+                                 // only Participants  can add the other Member
+                                
+                                 
+        participants.push( _member);
         
     }
     
+    function changeLeader(address payable _NewLeader)public onlyParticipants{
+        
+                                 // Member can change the Leader if something happen
+                                 // to current leader
+        
+         Leader = _NewLeader;
+    }
     
-    function selection (address _address) private {
+    
+    function selection (address _address) view private {
+        
+                        // checking wether the given address already benfitted or not
         
           if(Member_detail[_address].benfitted == true){
               
@@ -77,10 +117,19 @@ contract Union{
 
     
       function  withdrawal() onlyLeader  public{
+          
+                     // only leader can can can call this function
+          
+          
           require(time >=  100);
+                              
+                              // community member can choose  their  time span like 30 days, here i
+                              // have taken 100s 
           
            address selected_member =  participants[length];
            selection(selected_member);
+           
+                           // first we need to get approval before transfer of token
           
            (bool _success, bytes memory _data) = token.call(
                   abi.encodeWithSignature(
@@ -93,6 +142,8 @@ contract Union{
               require(_success);
               
               
+                           // after approval is done we have transfer the token from this
+                           // contract to its participants
           
           (bool success, bytes memory data) = token.call(
                   abi.encodeWithSignature(
@@ -103,13 +154,26 @@ contract Union{
                   )
               );
               
+                           // updating the Member_detail who got benfitted
+                           
               Member_detail[selected_member].benfitted = true;
               Member_detail[selected_member].time = now;
               
               length++;
               
+              
+                           // if all the Member got benfitted we can reiterate the whole process
+                           // by updating  their Details
+                           
               if(length == (participants.length - 1)){
-                  length = 0;
+                  
+                   length = 0;
+                   
+                   for(uint i = 0; i < participants.length; i++){
+                       
+                        Member_detail[participants[i]].benfitted = false;
+                        Member_detail[participants[i]].time = 0;
+                   }
               }
              
           
